b9c361a981346487f757044120436311713980ba
Justice Foster <omario2005x@gmail.com>
2025-03-10 21:21:37 -0400

    made revisions to the resolvers.ts

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,68 +1,60 @@
+import User, { IUser } from '../models/User.js';
+import Book, { IBook } from '../models/Book.js';
 import { AuthenticationError } from 'apollo-server-express';
-import User from '../models/User.js';
-import Book from '../models/Book.js';
 import { signToken } from '../services/auth.js';
 
-export const me = async (_: any, __: any, context: any) => {
-  if (context.user) {
-    return await User.findById(context.user._id).populate('savedBooks');
-  }
-  throw new AuthenticationError('Not logged in');
-};
-
-export const books = async () => {
-  return await Book.find();
-};
-
-export const login = async (_: any, { email, password }: { email: string, password: string }) => {
-  const user = await User.findOne({ email });
-
-  if (!user || !(await user.isCorrectPassword(password))) {
-    throw new AuthenticationError('Invalid credentials');
-  }
-
-  const token = signToken(user);
-  return { token, user };
-};
-
-export const addUser = async (_: any, { username, email, password }: { username: string, email: string, password: string }) => {
-  const user = await User.create({ username, email, password });
-  const token = signToken(user);
-  return { token, user };
-};
-
-export const saveBook = async (_: any, { book }: { book: any }, context: any) => {
-  if (context.user) {
-    return await User.findByIdAndUpdate(
-      context.user._id,
-      { $push: { savedBooks: book } },
-      { new: true }
-    ).populate('savedBooks');
-  }
-  throw new AuthenticationError('Not logged in');
-};
-
-export const removeBook = async (_: any, { bookId }: { bookId: string }, context: any) => {
-  if (context.user) {
-    return await User.findByIdAndUpdate(
-      context.user._id,
-      { $pull: { savedBooks: { _id: bookId } } },
-      { new: true }
-    ).populate('savedBooks');
-  }
-  throw new AuthenticationError('Not logged in');
-};
-
-// Export resolvers object
-export const resolvers = {
+const resolvers = {
   Query: {
-    me,
-    books,
+    me: async (_parent: any, _args: any, context: any): Promise<IUser | null> => {
+      if (context.user) {
+        return await User.findById(context.user._id).populate('savedBooks');
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    books: async (): Promise<IBook[] | null> => {
+      return await Book.find({});
+    },
   },
   Mutation: {
-    login,
-    addUser,
-    saveBook,
-    removeBook,
+    login: async (_parent: any, { email, password }: { email: string, password: string }): Promise<{ token: string; user: IUser } | null> => {
+      const user = await User.findOne({ email });
+
+      if (!user || !(await user.isCorrectPassword(password))) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (_parent: any, { username, email, password }: { username: string, email: string, password: string }): Promise<{ token: string; user: IUser } | null> => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (_parent: any, { book }: { book: IBook }, context: any): Promise<IUser | null> => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $push: { savedBooks: book } },
+          { new: true }
+        ).populate('savedBooks');
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    removeBook: async (_parent: any, { bookId }: { bookId: string }, context: any): Promise<IUser | null> => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { _id: bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+      }
+      throw new AuthenticationError('Not logged in');
+    },
   },
 };
+
+export default resolvers;
